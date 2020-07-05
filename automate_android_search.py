0e3929a1d52e889c6bd226a8222cbb5b49389e61
Akhilesh <49302108+akhileshkoti@users.noreply.github.com>
2020-07-05 15:05:27 +0530

    Create automate_android_search.py

---
+++ automate_android_search.py
@@ -0,0 +1,20 @@
+from ppadb.client import Client
+import clipboard as c
+import time
+client=Client(host="127.0.0.1",port=5037)
+print("------------Starting Server------------")
+print("Connecting to client",end='')
+for i in range(3):
+    print('.',end='')
+    time.sleep(1)
+devices=client.devices()
+device=devices[0]
+print('Connected\n')
+while(True):
+        text=c.paste()
+        device.shell('input keyevent 84')
+        time.sleep(2)
+        device.shell('input text '+text)
+        print(text)
+        device.shell('input keyevent 28')
+        device.shell('input keyevent 66')
